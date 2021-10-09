2c4a9e26cc9191669339c38d9fd8c16e1c1c1e6a
falkTX <falktx@falktx.com>
2021-10-09 23:26:53 +0100

    Add header comments; Alternative x86 linux build; Fix mac build
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/Cardinal/GL/glew.h
+++ plugins/Cardinal/GL/glew.h
@@ -1,3 +1,20 @@
+/*
+ * DISTRHO Cardinal Plugin
+ * Copyright (C) 2021 Filipe Coelho <falktx@falktx.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For a full copy of the GNU General Public License see the LICENSE file.
+ */
+
 #pragma once
 
 #ifdef NDEBUG
