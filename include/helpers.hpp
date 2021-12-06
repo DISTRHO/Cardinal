4ec34e1f805d08e8d61f2a4f21dacd150c1b48dd
falkTX <falktx@falktx.com>
2021-12-06 22:15:25 +0000

    Avoid VOID name conflict with Windows headers
    Signed-off-by: falkTX <falktx@falktx.com>

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -119,3 +119,6 @@ CardinalPluginModel<TModule, TModuleWidget>* createModel(std::string slug)
 #define createModel createModelOldVCV
 #include_next "helpers.hpp"
 #undef createModel
+
+// this is defined by Windows headers, conflicts with Bidoo VOID module name
+#undef VOID
