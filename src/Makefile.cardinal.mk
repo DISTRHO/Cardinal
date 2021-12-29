4e695f3a1329f4722722f7f4fc1fc9f20bb9ddef
falkTX <falktx@falktx.com>
2021-12-29 22:35:46 +0000

    Stop generating FX and Synth variants for jack standalone builds
    Signed-off-by: falkTX <falktx@falktx.com>

--- src/Makefile.cardinal.mk
+++ src/Makefile.cardinal.mk
@@ -240,7 +240,7 @@ BUILD_CXX_FLAGS += -DCARDINAL_PLUGIN_PREFIX='"$(PREFIX)"'
 
 ifeq ($(NAME),CardinalFX)
 
-all: jack lv2 vst2 vst3 resources
+all: lv2 vst2 vst3 resources
 
 CORE_RESOURCES = $(filter-out icon.png,$(subst ../Rack/res/,,$(wildcard ../Rack/res/*))) template.vcv
 
@@ -257,7 +257,7 @@ else # CardinalFX
 ifeq ($(NAME),Cardinal)
 all: jack lv2 vst3 resources
 else
-all: jack lv2 vst2 vst3 resources
+all: lv2 vst2 vst3 resources
 endif
 
 PLUGIN_RESOURCES += $(TARGET_DIR)/$(NAME).lv2/resources
