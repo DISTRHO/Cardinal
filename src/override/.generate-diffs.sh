810928190c6bbb38d21e6fa52e4a2fcd511af23a
falkTX <falktx@falktx.com>
2022-01-23 17:19:14 +0000

    Add diffs for the overriden files
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ src/override/.generate-diffs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+diff -U3 ../Rack/dep/oui-blendish/blendish.c blendish.c > diffs/blendish.c.diff
+diff -U3 ../Rack/src/common.cpp common.cpp > diffs/common.cpp.diff
+diff -U3 ../Rack/src/context.cpp context.cpp > diffs/context.cpp.diff
+diff -U3 ../Rack/src/app/MenuBar.cpp MenuBar.cpp > diffs/MenuBar.cpp.diff
+diff -U3 ../Rack/src/app/Scene.cpp Scene.cpp > diffs/Scene.cpp.diff
+diff -U3 ../Rack/src/engine/Engine.cpp Engine.cpp > diffs/Engine.cpp.diff
+diff -U3 ../Rack/src/plugin/Model.cpp Model.cpp > diffs/Model.cpp.diff
+diff -U3 ../Rack/src/window/Window.cpp Window.cpp > diffs/Window.cpp.diff
