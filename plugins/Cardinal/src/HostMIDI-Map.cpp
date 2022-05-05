c8772d46545a66b9ab4039e7e04e4b1d59e2d00a
falkTX <falktx@falktx.com>
2022-05-05 12:03:23 +0100

    Only print errors of HostMIDI-Map once
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/Cardinal/src/HostMIDI-Map.cpp
+++ plugins/Cardinal/src/HostMIDI-Map.cpp
@@ -602,10 +602,10 @@ struct CardinalMIDIMapChoice : CardinalLedDisplayChoice {
         ParamHandle* const paramHandle = &module->paramHandles[id];
 
         Module* const paramModule = paramHandle->module;
-        DISTRHO_SAFE_ASSERT_RETURN(paramModule != nullptr, "error");
+        DISTRHO_CUSTOM_SAFE_ASSERT_ONCE_RETURN(paramModule != nullptr, "error");
 
         const int paramId = paramHandle->paramId;
-        DISTRHO_SAFE_ASSERT_RETURN(paramId < (int) paramModule->params.size(), "error");
+        DISTRHO_CUSTOM_SAFE_ASSERT_ONCE_RETURN(paramId < (int) paramModule->params.size(), "error");
 
         ParamQuantity* const paramQuantity = paramModule->paramQuantities[paramId];
         std::string s = paramQuantity->name;
