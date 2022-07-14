d7d85606f44beda5d58695eb3bcfcdb1adc1ea2e
falkTX <falktx@falktx.com>
2022-07-14 11:27:44 +0100

    Enable full wasm build
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ src/override/OpenGlWidget.cpp
@@ -0,0 +1,48 @@
+/*
+ * DISTRHO Cardinal Plugin
+ * Copyright (C) 2021-2022 Filipe Coelho <falktx@falktx.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * For a full copy of the GNU General Public License see the LICENSE file.
+ */
+
+/**
+ * This file is an edited version of VCVRack's OpenGlWidget.cpp
+ * Copyright (C) 2016-2021 VCV.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ */
+
+#include <widget/OpenGlWidget.hpp>
+#include <context.hpp>
+
+
+namespace rack {
+namespace widget {
+
+
+void OpenGlWidget::step() {
+	// Render every frame
+	dirty = true;
+	FramebufferWidget::step();
+}
+
+
+void OpenGlWidget::drawFramebuffer() {
+}
+
+
+} // namespace widget
+} // namespace rack
