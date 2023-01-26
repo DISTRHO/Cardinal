69ab3c2de7a4c84984c53612b13fd4648a36543e
falkTX <falktx@falktx.com>
2023-01-26 17:43:09 +0100

    Tweaks to mini modules, add a few more surgext ones
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/plugins-mini.cpp
+++ plugins/plugins-mini.cpp
@@ -532,61 +532,38 @@ static void initStatic__surgext()
     if (spl.ok())
     {
         p->addModel(modelVCOClassic);
-//         p->addModel(modelVCOModern);
-//         p->addModel(modelVCOWavetable);
-//         p->addModel(modelVCOWindow);
-//         p->addModel(modelVCOSine);
-//         p->addModel(modelVCOFM2);
-//         p->addModel(modelVCOFM3);
-//         p->addModel(modelVCOSHNoise);
-//         p->addModel(modelVCOAlias);
-//         p->addModel(modelVCOString);
-//         p->addModel(modelVCOTwist);
-        spl.removeModule("SurgeXTOSCModern");
-        spl.removeModule("SurgeXTOSCWavetable");
-        spl.removeModule("SurgeXTOSCWindow");
-        spl.removeModule("SurgeXTOSCSine");
+        p->addModel(modelVCOModern);
+        p->addModel(modelVCOSHNoise);
+        p->addModel(modelVCOSine);
+        p->addModel(modelVCOString);
+        p->addModel(modelVCOTwist);
+        /*
+        p->addModel(modelVCOAlias);
+        p->addModel(modelVCOFM2);
+        p->addModel(modelVCOFM3);
+        p->addModel(modelVCOWavetable);
+        p->addModel(modelVCOWindow);
+        */
+        spl.removeModule("SurgeXTOSCAlias");
         spl.removeModule("SurgeXTOSCFM2");
         spl.removeModule("SurgeXTOSCFM3");
-        spl.removeModule("SurgeXTOSCSHNoise");
-        spl.removeModule("SurgeXTOSCAlias");
-        spl.removeModule("SurgeXTOSCString");
-        spl.removeModule("SurgeXTOSCTwist");
+        spl.removeModule("SurgeXTOSCWavetable");
+        spl.removeModule("SurgeXTOSCWindow");
 
         // Add the ported ones
-//         p->addModel(modelSurgeVCF);
-//         p->addModel(modelSurgeDelay);
-//         p->addModel(modelSurgeDelayLineByFreq);
-//         p->addModel(modelSurgeWaveshaper);
-//         p->addModel(modelSurgeLFO);
-//         p->addModel(modelSurgeMixer);
-//         p->addModel(modelSurgeModMatrix);
-        spl.removeModule("SurgeXTVCF");
+        p->addModel(modelSurgeLFO);
+        p->addModel(modelSurgeMixer);
+        p->addModel(modelSurgeModMatrix);
+        p->addModel(modelSurgeWaveshaper);
+        /*
+        p->addModel(modelSurgeDelay);
+        p->addModel(modelSurgeDelayLineByFreq);
+        p->addModel(modelSurgeVCF);
+        */
         spl.removeModule("SurgeXTDelay");
         spl.removeModule("SurgeXTDelayLineByFreq");
-        spl.removeModule("SurgeXTWaveshaper");
-        spl.removeModule("SurgeXTLFO");
-        spl.removeModule("SurgeXTMixer");
-        spl.removeModule("SurgeXTModMatrix");
+        spl.removeModule("SurgeXTVCF");
 
-//         p->addModel(modelFXReverb);
-//         p->addModel(modelFXPhaser);
-//         p->addModel(modelFXRotarySpeaker);
-//         p->addModel(modelFXDistortion);
-//         p->addModel(modelFXFrequencyShifter);
-//         p->addModel(modelFXChorus);
-//         p->addModel(modelFXVocoder);
-//         p->addModel(modelFXReverb2);
-//         p->addModel(modelFXFlanger);
-//         p->addModel(modelFXRingMod);
-//         p->addModel(modelFXNeuron);
-//         p->addModel(modelFXResonator);
-//         p->addModel(modelFXChow);
-//         p->addModel(modelFXExciter);
-//         p->addModel(modelFXEnsemble);
-//         p->addModel(modelFXCombulator);
-//         p->addModel(modelFXSpringReverb);
-//         p->addModel(modelFXTreeMonster);
         spl.removeModule("SurgeXTFXChorus");
         spl.removeModule("SurgeXTFXChow");
         spl.removeModule("SurgeXTFXCombulator");
