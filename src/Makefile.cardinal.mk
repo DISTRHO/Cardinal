120d5e3c0840daec39ccb688a1036125ff133d31
falkTX <falktx@falktx.com>
2023-03-05 14:43:48 +0100

    Use carla single static lib setup
    Signed-off-by: falkTX <falktx@falktx.com>

--- src/Makefile.cardinal.mk
+++ src/Makefile.cardinal.mk
@@ -25,31 +25,8 @@ ifneq ($(CARDINAL_VARIANT),mini)
 ifneq ($(STATIC_BUILD),true)
 
 STATIC_PLUGIN_TARGET = true
-
-CWD = ../../carla/source
-include $(CWD)/Makefile.deps.mk
-
-CARLA_BUILD_DIR = ../../carla/build
-ifeq ($(DEBUG),true)
-CARLA_BUILD_TYPE = Debug
-else
-CARLA_BUILD_TYPE = Release
-endif
-
-CARLA_EXTRA_LIBS  = $(CARLA_BUILD_DIR)/plugin/$(CARLA_BUILD_TYPE)/carla-host-plugin.cpp.o
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/carla_engine_plugin.a
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/carla_plugin.a
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/native-plugins.a
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/audio_decoder.a
-ifneq ($(WASM),true)
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/jackbridge.min.a
-endif
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/lilv.a
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/rtmempool.a
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/sfzero.a
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/water.a
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/ysfx.a
-CARLA_EXTRA_LIBS += $(CARLA_BUILD_DIR)/modules/$(CARLA_BUILD_TYPE)/zita-resampler.a
+CARLA_EXTRA_LIBS = ../../carla/bin/libcarla_host-plugin.a
+include ../../carla/source/Makefile.deps.mk
 
 endif # STATIC_BUILD
 endif # CARDINAL_VARIANT mini
