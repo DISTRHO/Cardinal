62bacc69acf291cfaf6ac5bfebed9fb48511df9d
washikano <shiera.wa.kaimononiiku@gmail.com>
2024-04-26 02:31:10 -0600

    Add LOGinstruments to Makefile and plugins.cpp

--- plugins/plugins.cpp
+++ plugins/plugins.cpp
@@ -587,6 +587,10 @@ extern Model* modelDriftgen;
 extern Model* modelLooperOne;
 extern Model* modelLooperTwo;
 
+// LOGinstruments
+#include "LOGinstruments/src/kiss_fft.c"
+#include "LOGinstruments/src/LOGinstruments.hpp"
+
 // LomasModules
 #include "LomasModules/src/plugin.hpp"
 #undef DR_WAV_IMPLEMENTATION
@@ -933,6 +937,7 @@ Plugin* pluginInstance__kocmoc;
 Plugin* pluginInstance__LifeFormModular;
 Plugin* pluginInstance__LilacLoop;
 Plugin* pluginInstance__LittleUtils;
+Plugin* pluginInstance__LOGinstruments;
 Plugin* pluginInstance__Lomas;
 Plugin* pluginInstance__Lyrae;
 Plugin* pluginInstance__Meander;
@@ -2551,6 +2556,21 @@ static void initStatic__LittleUtils()
     }
 }
 
+static void initStatic__LOGinstruments()
+{
+    Plugin* const p = new Plugin;
+    pluginInstance__LOGinstruments = p;
+
+    const StaticPluginLoader spl(p, "LOGinstruments");
+    if (spl.ok())
+    {
+        p->addModel(modelSpeck);
+		p->addModel(modelLessMess);
+		p->addModel(modelVelvet);
+		p->addModel(modelCrystal);
+    }
+}
+
 static void initStatic__Lomas()
 {
     Plugin* const p = new Plugin;
@@ -3414,6 +3434,7 @@ void initStaticPlugins()
     initStatic__LifeFormModular();
     initStatic__LilacLoop();
     initStatic__LittleUtils();
+    initStatic__LOGinstruments();
     initStatic__Lomas();
     initStatic__Lyrae();
     initStatic__Meander();
