9ed8f340257d841731225ed8674995b84fef6e13
falkTX <falktx@falktx.com>
2024-05-24 12:23:57 +0200

    Make sure to use custom JUCE version for surgext builds
    Signed-off-by: falkTX <falktx@falktx.com>

--- src/Makefile.cardinal.mk
+++ src/Makefile.cardinal.mk
@@ -188,7 +188,7 @@ endif
 ifeq ($(MACOS),true)
 EXTRA_DSP_LIBS += -framework Accelerate -framework AppKit
 else ifeq ($(WINDOWS),true)
-EXTRA_DSP_LIBS += -lole32 -lshlwapi -luuid -lversion -lwininet
+EXTRA_DSP_LIBS += -lole32 -lshlwapi -luuid -lversion
 endif
 
 # --------------------------------------------------------------
