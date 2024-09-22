024ec1ef89d9eaa9039b7cccc73bc62f9c912727
falkTX <falktx@falktx.com>
2024-09-22 15:06:58 +0200

    Update voxglitch
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/plugins.cpp
+++ plugins/plugins.cpp
@@ -3350,26 +3350,33 @@ static void initStatic__Voxglitch()
     if (spl.ok())
     {
 #define modelLooper modelVoxglitchLooper
-      p->addModel(modelAutobreak);
-      p->addModel(modelByteBeat);
-      p->addModel(modelDigitalProgrammer);
-      p->addModel(modelDigitalSequencer);
-      p->addModel(modelDigitalSequencerXP);
-      p->addModel(modelGlitchSequencer);
-      p->addModel(modelGhosts);
-      p->addModel(modelGrainEngineMK2);
-      p->addModel(modelGrainEngineMK2Expander);
-      p->addModel(modelGrainFx);
-      p->addModel(modelGrooveBox);
-      p->addModel(modelGrooveBoxExpander);
-      p->addModel(modelHazumi);
-      p->addModel(modelLooper);
-      p->addModel(modelRepeater);
-      p->addModel(modelSamplerX8);
-      p->addModel(modelSatanonaut);
-      p->addModel(modelWavBank);
-      p->addModel(modelWavBankMC);
-      p->addModel(modelXY);
+        p->addModel(modelArpSeq);
+        p->addModel(modelAutobreak);
+        p->addModel(modelAutobreakStudio);
+        p->addModel(modelByteBeat);
+        p->addModel(modelDigitalProgrammer);
+        p->addModel(modelDigitalSequencer);
+        p->addModel(modelDigitalSequencerXP);
+        p->addModel(modelDrumRandomizer);
+        p->addModel(modelGlitchSequencer);
+        p->addModel(modelGhosts);
+        p->addModel(modelGrainEngineMK2);
+        p->addModel(modelGrainEngineMK2Expander);
+        p->addModel(modelGrainFx);
+        p->addModel(modelGrooveBox);
+        p->addModel(modelGrooveBoxExpander);
+        p->addModel(modelHazumi);
+        p->addModel(modelOnePoint);
+        p->addModel(modelOneZero);
+        p->addModel(modelLooper);
+        p->addModel(modelNoteDetector);
+        p->addModel(modelRepeater);
+        p->addModel(modelSamplerX8);
+        p->addModel(modelSampler16P);
+        p->addModel(modelSatanonaut);
+        p->addModel(modelWavBank);
+        p->addModel(modelWavBankMC);
+        p->addModel(modelXY);
 #undef modelLooper
     }
 }
