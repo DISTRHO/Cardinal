c2245dcd639f4d8cb19e192265fc378b9c2248e1
dromer <alexander@puikheid.nl>
2024-11-18 14:36:27 +0100

    update Befaco to v2.8.0

--- src/custom/dep.cpp
+++ src/custom/dep.cpp
@@ -440,6 +440,8 @@ static const struct {
     { kModeBefaco, "/Befaco/res/panels/StereoStrip.svg" },
     { kModeBefaco, "/Befaco/res/panels/Voltio.svg" },
     { kModeBefaco, "/Befaco/res/panels/Octaves.svg" },
+    { kModeBefaco, "/Befaco/res/panels/Bypass.svg" },
+    { kModeBefaco, "/Befaco/res/panels/Bandit.svg" },
     // GPLv3+
     { kModeCardinal, "/Cardinal/res/AudioFile.svg" },
     { kModeCardinal, "/Cardinal/res/AudioToCVPitch.svg" },
