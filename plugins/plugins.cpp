a3779576a2831784bca6e67a69ff7a9d3d0b6f3d
rl2939 <60378449+rl2939@users.noreply.github.com>
2026-02-10 13:06:20 -0500

    Update Autinn.

--- plugins/plugins.cpp
+++ plugins/plugins.cpp
@@ -106,6 +106,7 @@ extern Model* modelBlank;
 */
 #define modelChord modelAutinnChord
 #define modelVibrato modelAutinnVibrato
+#define modelSnare modelAutinnSnare
 extern Model* modelJette;
 extern Model* modelFlora;
 extern Model* modelOxcart;
@@ -130,8 +131,14 @@ extern Model* modelFil;
 extern Model* modelNap;
 extern Model* modelMelody;
 extern Model* modelChord;
+extern Model* modelKicker;
+extern Model* modelSnare;
+extern Model* modelCoil;
+extern Model* modelGeiger;
+extern Model* modelSaw2;
 #undef modelChord
 #undef modelVibrato
+#undef modelSnare
 
 // Axioma
 #include "Axioma/src/plugin.hpp"
@@ -1554,6 +1561,7 @@ static void initStatic__Autinn()
     {
 #define modelChord modelAutinnChord
 #define modelVibrato modelAutinnVibrato
+#define modelSnare modelAutinnSnare
         p->addModel(modelAmp);
         p->addModel(modelDeadband);
         p->addModel(modelBass);
@@ -1578,8 +1586,14 @@ static void initStatic__Autinn()
         p->addModel(modelNap);
         p->addModel(modelMelody);
         p->addModel(modelChord);
+        p->addModel(modelKicker);
+	    p->addModel(modelSnare);
+	    p->addModel(modelCoil);
+	    p->addModel(modelGeiger);
+	    p->addModel(modelSaw2);
 #undef modelChord
 #undef modelVibrato
+#undef modelSnare
     }
 }
 
